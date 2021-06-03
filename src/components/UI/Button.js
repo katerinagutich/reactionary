ad84e1bbf5defa93f7382b8f7144e87dddd95c36
Kate <katerina_gutich@mail.ru>
2021-06-03 21:56:06 +0300

    ADD: add onClick handler and class to the button

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -2,7 +2,12 @@ import styles from './Button.module.css'
 
 const Button = (props) => {
   return (
-    <button onClick={props.onClick} className={styles.button} type={props.type}>
+    <button
+      onClick={props.onClick}
+      className={styles.button}
+      type={props.type}
+      disabled={props.disabled}
+    >
       {props.name}
     </button>
   )
