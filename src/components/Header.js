d25d531b406ea06df1dec467cc8a3fe92678e87b
Kate <katerina_gutich@mail.ru>
2021-06-03 17:16:16 +0300

    NEW: create Header component and add styling for it

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import { useDispatch, useSelector } from 'react-redux'
+
+import { authActions } from '../store/auth-slice'
+import styles from './Header.module.css'
+
+const Header = () => {
+  const dispatch = useDispatch()
+  const isAuth = useSelector((state) => state.authentication.isAuthenticated)
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout())
+  }
+
+  return (
+    <header className={styles.header}>
+      <a href="/" className={styles.logo}>
+        Reactionary
+      </a>
+      {isAuth && (
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Main Page</a>
+            </li>
+            <li>
+              <a href="/">My List</a>
+            </li>
+            <li>
+              <button onClick={logoutHandler}>Log Out</button>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </header>
+  )
+}
+export default Header
