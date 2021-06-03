264eca612d28358d299db9f7c0b4bc872a15ea96
Kate <katerina_gutich@mail.ru>
2021-06-03 22:03:55 +0300

    ADD: add useInput hook to validate user inputs

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,18 +1,61 @@
+import useInput from '../hooks/use-input'
 import styles from './LoginForm.module.css'
 import Button from './UI/Button'
 
 const LoginForm = () => {
+  const {
+    value: name,
+    isValid: nameIsValid,
+    hasError: nameHasError,
+    valueChangeHandler: nameChangeHandler,
+    inputBlurHandler: nameBlurHandler,
+    reset: resetName,
+  } = useInput((value) => value.trim().length > 2)
+
+  const {
+    value: password,
+    isValid: passwordIsValid,
+    hasError: passwordHasError,
+    valueChangeHandler: passwordChangeHandler,
+    inputBlurHandler: passwordBlurHandler,
+    reset: resetPassword,
+  } = useInput((value) => value.trim().length > 5)
+
+  let formIsValid
+  nameIsValid && passwordIsValid ? (formIsValid = true) : (formIsValid = false)
+
+  const onLoginHandler = (event) => {
+    event.preventDefault()
+    if (!formIsValid) return
+    resetName()
+    resetPassword()
+  }
+
   return (
-    <form>
+    <form onSubmit={onLoginHandler}>
       <div className={styles.control}>
         <label htmlFor="name">Your Name: </label>
-        <input id="name" type="text" />
+        <input
+          value={name}
+          onChange={nameChangeHandler}
+          onBlur={nameBlurHandler}
+          id="name"
+          type="text"
+          className={nameHasError ? styles.invalid : ''}
+        />
       </div>
       <div className={styles.control}>
-        <label>Your Password: </label>
-        <input type="password" />
+        <label htmlFor="password">Your Password: </label>
+        <input
+          id="password"
+          value={password}
+          onChange={passwordChangeHandler}
+          onBlur={passwordBlurHandler}
+          type="password"
+          className={passwordHasError ? styles.invalid : ''}
+        />
       </div>
-      <Button name="Let Me In" />
+      <Button disabled={!formIsValid} name="Let Me In" />
     </form>
   )
 }
