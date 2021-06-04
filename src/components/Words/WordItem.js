40bddf0476e355dd91dd39887ddb3dc3e3c8ac0c
Kate <katerina_gutich@mail.ru>
2021-06-04 17:34:52 +0300

    NEW: create WordItem component to render details for the word selected by user from the list, add styling to component

---
+++ src/components/Words/WordItem.js
@@ -0,0 +1,11 @@
+import styles from './WordItem.module.css'
+
+const WordItem = (props) => {
+  return (
+    <figure className={styles}>
+      <div />
+    </figure>
+  )
+}
+
+export default WordItem
