e6f90a9c8f4f7676ab7006401657a7931d22904e
Kate <katerina_gutich@mail.ru>
2021-06-04 17:36:26 +0300

    ADD: add React Router to render separate pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { Switch, Route, Redirect } from 'react-router-dom'
+
 import Header from './components/Header'
 import Home from './components/Home'
+import WordList from './components/Words/WordList'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/reactionary" />
+        </Route>
+        <Route path="/reactionary" exact>
+          <Home />
+        </Route>
+        <Route path="/reactionary/word-list" exact>
+          <WordList />
+        </Route>
+        <Route path="/reactionary/word-list/:wordId">
+          <WordList />
+        </Route>
+      </Switch>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 
 import store from './store/index'
 import './index.css'
 import App from './App'
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root')
 )
