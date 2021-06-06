af1c3136256c386f9562fda19c3598ba1b5b367b
Kate <katerina_gutich@mail.ru>
2021-06-07 01:45:12 +0300

    ADD: useHttp hook to get word list from Firebase

--- src/components/Words/WordList.js
+++ src/components/Words/WordList.js
@@ -1,38 +1,34 @@
+import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
+import useHttp from '../../hooks/use-http'
+import { getWordList } from '../api/api'
 import styles from './WordList.module.css'
+import NoWordsFound from './NoWordsFound'
 
 const WordList = () => {
-  const wordList = [
-    {
-      word: 'dictionary',
-      transcription: '/ˈdɪkʃəneri/',
-      definition:
-        'A book or electronic resource that lists the words of a language (typically in alphabetical order) and gives their meaning, or gives the equivalent words in a different language, often also providing information about pronunciation, origin, and usage.',
-    },
-    {
-      word: 'website',
-      transcription: '/ˈwɛbsaɪt/',
-      definition:
-        'A set of related web pages located under a single domain name, typically produced by a single person or organization.',
-    },
-    {
-      word: 'store',
-      transcription: '/stɔr/',
-      definition: 'Keep or accumulate (something) for future use.',
-    },
-    {
-      word: 'book',
-      transcription: '/bʊk/',
-      definition:
-        'Reserve (accommodations, a place, etc.); buy (a ticket) in advance.',
-    },
-  ]
+  const { sendRequest, status, data: loadedWords, error } = useHttp(getWordList)
+
+  useEffect(() => {
+    sendRequest()
+  }, [sendRequest])
+
+  if (status === 'pending') {
+    return <div>Pending...</div>
+  }
+
+  if (error) {
+    return <p>{error}</p>
+  }
+
+  if (status === 'completed' && (!loadedWords || loadedWords.length === 0)) {
+    return <NoWordsFound />
+  }
 
   return (
     <div className={styles.list}>
-      {wordList.map((item) => (
-        <section key={item.word} className={styles.item}>
+      {loadedWords.map((item) => (
+        <section key={item.id} className={styles.item}>
           <div className={styles.word}>{item.word}</div>
           <div className={styles.transcription}>{item.transcription}</div>
           <Link className="button" to={`/word-list/${item.word}`}>
